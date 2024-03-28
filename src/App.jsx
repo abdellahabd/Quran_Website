48586d1133cf690956be913018a3ea921c506970
abdellahabd <99611751+abdellahabd@users.noreply.github.com>
2024-03-28 23:50:24 +0100

    secend commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import Nav from "./Comp/NavigationBar";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const audio = new Audio("/src/assets/videoplayback.mp3");
+    audio.play();
+
+    return () => {
+      audio.pause();
+      // audio.src = "";
+      // audio.load();
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Nav />
+      <div
+        onClick={() => {
+          const audio = new Audio("/src/assets/videoplayback.mp3");
+          audio.play();
+        }}
+      >
+        hello
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
